1fb808fb1231d82baad62c0bc8e80552e20df39c
hgs-study <hgstudy_@naver.com>
2021-07-18 14:49:21 +0900

    [문자열 덧셈 계산기] 3. 리팩토링 : 메소드 분리(indent 1로 변경)

--- src/main/java/com/hgstudy/stringCalculator/StringCalculator.java
+++ src/main/java/com/hgstudy/stringCalculator/StringCalculator.java
@@ -9,11 +9,19 @@ public class StringCalculator {
             result = 0;
         }else {
             String[] values = text.split(",|:");
-            for (String value : values) {
-                result += Integer.parseInt(value);
-            }
+            result = sum(values); // 메소드 분리 : indent 1로 변경
         }
         return result;
     }
+
+    // 메소드 분리 : indent 1로 변경
+    private static int sum(String[] values) {
+        int result = 0;
+        for (String value : values) {
+            result += Integer.parseInt(value);
+        }
+        return result;
+    }
+
 
 }
